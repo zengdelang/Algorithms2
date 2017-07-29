09c4b9f322ff566b8f5f88c99285a49ba2029c32
zengdelang <1147250901@qq.com>
2017-07-29 10:37:51 +0800

    Add files via upload

---
+++ Algorithms/CycleDetection/CycleDetection.cs
@@ -0,0 +1,46 @@
+﻿public class TestList
+{
+    public int Value;
+    public TestList Next;
+}
+
+public class CycleDetection
+{
+    /// <summary>
+    /// 检测链表是否有环，有则返回环的开头，否则为null
+    /// </summary>
+    /// <param name="list"></param>
+    /// <returns></returns>
+    public static TestList DetectCycle(TestList list)
+    {
+        var fast = list;
+        var slow = fast;
+        bool isCircular = true;
+
+        do
+        {
+            if (fast.Next == null || fast.Next.Next == null)
+            {
+                isCircular = false;
+                break;
+            }
+
+            slow = slow.Next;
+            fast = fast.Next.Next;
+
+        } while (fast != slow);
+
+        if (!isCircular)
+            return null;
+
+        slow = list;
+        while (slow != fast)
+        {
+            slow = slow.Next;
+            fast = fast.Next;
+        }
+
+        return slow;
+    }
+}
+
